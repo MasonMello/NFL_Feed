ba86485506ebac1f6c8adcb78b91d63abd8d8251
jklins-ects <73347118+jklins-ects@users.noreply.github.com>
2025-03-12 12:19:55 -0400

    proxy working

--- nfl-feed/src/components/SingleArticle.jsx
+++ nfl-feed/src/components/SingleArticle.jsx
@@ -1,38 +1,49 @@
-import { getArticle } from "../services/api";
-import { useState, useEffect } from "react";
+import { getArticle } from '../services/api';
+import { useState, useEffect } from 'react';
 
-function SingleArticle({articleID}){
-    const [article, setArticle] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+function SingleArticle({ articleID }) {
+  const [article, setArticle] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [story, setStory] = useState('');
 
-    useEffect(() => {
-        const loadArticle = async () => {
-            try {
-                setLoading(true);
-                const articleData = await getArticle(articleID);
-                setArticle(articleData);
-                setError(null);
-            } catch (err) {
-                setError("Failed to load article");
-                console.log(err);
-            } finally {
-                setLoading(false);
-            }
-        }
-        loadArticle();
-    }, [])
+  useEffect(() => {
+    const loadArticle = async () => {
+      try {
+        setLoading(true);
+        const articleData = await getArticle(articleID);
+        console.log(articleData);
+        setArticle(articleData);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>{error}</p>;
-    console.log(article);
+        setError(null);
+      } catch (err) {
+        setError('Failed to load article');
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadArticle();
+  }, []);
 
-    return (
-        <>
-        <h1>{article.headline}</h1>
-        </>
-        
-    );
+  useEffect(() => {
+    if (article?.headlines?.[0]?.story) {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(
+        article.headlines[0].story,
+        'text/html'
+      );
+      setStory(doc.body.innerHTML);
+    } else {
+      setStory(null); // or a suitable fallback state
+    }
+  }, [article]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  //console.log(article);
+
+  return <div dangerouslySetInnerHTML={{ __html: story }} />;
 }
 
 export default SingleArticle;
--- nfl-feed/src/services/api.js
+++ nfl-feed/src/services/api.js
@@ -1,27 +1,28 @@
-const articlesURL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news";
+const articlesURL =
+  'https://site.api.espn.com/apis/site/v2/sports/football/nfl/news';
 
 export const getAllArticles = async () => {
-    const response = await fetch(articlesURL, {
-        mode: 'cors',
-    });
-    const data = await response.json();
-    return data.articles;
-}
+  const response = await fetch(articlesURL, {
+    mode: 'cors',
+  });
+  const data = await response.json();
+  return data.articles;
+};
 
-const scoresURL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard";
+const scoresURL =
+  'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard';
 export const getGames = async () => {
-    const response = await fetch(scoresURL, {
-        mode: 'cors',
-    });
-    const data = await response.json();
-    return data.events;
-}
+  const response = await fetch(scoresURL, {
+    mode: 'cors',
+  });
+  const data = await response.json();
+  return data.events;
+};
 
-
-const ARTICLE_PROXY_URL = 'http://localhost:3001/api/article';
+const ARTICLE_PROXY_URL = 'http://localhost:5000/api/article';
 
 export const getArticle = async (articleID) => {
   const response = await fetch(`${ARTICLE_PROXY_URL}/${articleID}`);
   const data = await response.json();
   return data;
-}
+};
