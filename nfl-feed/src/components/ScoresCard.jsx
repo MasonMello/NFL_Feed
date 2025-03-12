bc99bae56dff4ad2569d237ef4ee9c0be6fbfa42
MasonMello <masonmello2006@gmail.com>
2025-03-12 10:39:19 -0400

    runnable

--- nfl-feed/src/components/ScoresCard.jsx
+++ nfl-feed/src/components/ScoresCard.jsx
@@ -10,7 +10,7 @@ function ScoresCard({ game }) {
             </div>
             <div className="card-body">
                 <h5 className="card-title">
-                    <img src={game.competitions[0].competitors[0].}/>
+                    <img src={game.competitions[0].competitors[0]}/>
                     {game.competitions[0].competitors[0].team.name} {game.competitions[0].competitors[0].score} - {game.competitions[0].competitors[1].team.name} {game.competitions[0].competitors[1].score}</h5>
                 <p className="card-text">{game.gameClock}</p>
                 <p className="card-text"><small className="text-muted">{`Last Updated: ${game.lastUpdated}`}</small></p>
